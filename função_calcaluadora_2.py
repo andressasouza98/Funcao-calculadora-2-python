f7bb1d00a9bc356f8f4aa117fb3052b0454680f3
andressasouza98 <154765550+andressasouza98@users.noreply.github.com>
2024-08-12 08:47:41 -0300

    Add files via upload

---
+++ função_calcaluadora_2.py
@@ -0,0 +1,30 @@
+def calculadora(num1, num2, operacao):
+    if(operacao == 1):
+        return num1 + num2
+    elif(operacao == 2):
+        return num1 - num2
+    elif(operacao == 3):
+        return num1 * num2
+    elif(operacao == 4):
+        return num1 / num2
+    else :
+        return 0
+    
+    executar = True
+
+    while(executar == True):
+        print("Qual operação você quer realizar?")
+        print("1: soma, 2: subtração, 3: ultiplicação, 4: divisão, 0: sair")
+        operacao = int(input())
+        if(operacao < 0) or (operacao > 4):
+            print("Essa opção não existe")
+        elif(operacao == 0):
+            executar = False
+        else :
+            print("Insira o primeiro número da operação")
+            num1 = int(input())
+            print("Insira o segundo número da operação")
+            num2 = int(input())
+            resultado = calculadora(num1, num2, operacao)
+            print("O resultado é", resultado)
+                   
